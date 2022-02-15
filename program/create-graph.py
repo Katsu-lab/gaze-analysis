7d8f11a0002355b007cde51faf88c340e6a3e217
Katuya Fukuoka <katsu@mango-byte.com>
2022-02-15 13:38:49 +0900

    update: use scat

--- program/create-graph.py
+++ program/create-graph.py
@@ -7,15 +7,19 @@ data = pd.read_csv('../data/gaze.csv').values.tolist()
 
 def displayCoordinateGraph(x,y):
     fig = plt.figure()
-    ax1 = fig.add_subplot()
+    ax = fig.add_subplot()
     fig.patch.set_facecolor('#FFF')
-    ax1.patch.set_facecolor('#00FF00')
-    line = ax1.scatter(x,y,c='red',marker='.')
+    ax.patch.set_facecolor('#00FF00')
+    scat = ax.scatter(x,y,c='red',marker='.')
     plt.title('Gaze of a PC screen(From Bereaved family)')
     plt.xlim([0,3840])
     plt.ylim([0,2160])
     plt.show()
     # plt.colorbar()
+
+def implementAnimation(x,y,scat):
+    def update(i):
+        scat.set_data(x[:i], y[:i])
 
 def createScatterDiagram():
     x = []
