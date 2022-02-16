afec24f2855f75da85f8675050079737a28d6743
Katuya Fukuoka <katsu@mango-byte.com>
2022-02-16 14:52:00 +0900

    feat: added config file

--- program/plot-gaze-information.py
+++ program/plot-gaze-information.py
@@ -1,40 +1,50 @@
+import configparser
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-def createScatterDiagram(x,y):
+config = configparser.ConfigParser()
+config.read('config.ini', encoding='utf-8')
+
+def createScatterDiagram(x, y, t):
     figure = plt.figure()
     axes = figure.add_subplot()
     axes.patch.set_facecolor('#00FF00')
     axes.set_title('Gaze of a PC screen(From Bereaved family)')
     line, = plt.plot(x[0], y[0], c='red', marker='.')
-    plt.xlim([0,3840])
-    plt.ylim([0,2160])
+    plt.xlim([0, config.getint('PC', 'WidthDpi')])
+    plt.ylim([0, config.getint('PC', 'HeightDpi')])
 
     def update(i):
+        # if i != 0:
+        #     plt.cla()
+
         line.set_data(x[:i], y[:i])
 
-    ani = animation.FuncAnimation(figure, update)
+    ani = animation.FuncAnimation(figure, update, interval = 30)
     plt.show()
 
-def setCsvData():
-    data = pd.read_csv('../data/gaze.csv').values.tolist()
+def setCsvData(fileName):
+    data = pd.read_csv('../data/' + fileName).values.tolist()
 
     xCoordinateOnScreen = 0
     yCoordinateOnScreen = 1
     milliSecTimeInEyetracker = 2
     milliSecTimeInSystem = 3
-    startTimeInConversation = 4
+    elapsedTimeSinceBeginingOfConversation = 4
 
     x = [ column[xCoordinateOnScreen] for column in data ]
     y = [ column[yCoordinateOnScreen] for column in data ]
+    t = [ column[yCoordinateOnScreen] for column in data ]
 
-    return x,y
+    return x, y, t
 
 def displayScatterDiagram():
-    gaze = setCsvData()
+    gaze = setCsvData(config.get('GAZE CSV', 'Name'))
     xCoordinate = 0
     yCoordinate = 1
-    createScatterDiagram(gaze[xCoordinate],gaze[yCoordinate])
+    elapsedTime = 2
+    createScatterDiagram(gaze[xCoordinate], gaze[yCoordinate], gaze[elapsedTime])
 
 displayScatterDiagram()
