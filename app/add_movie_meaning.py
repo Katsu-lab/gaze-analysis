c16b105fc19906111a73df0f14a3ec7a4ff9f5aa
Katuya Fukuoka <katsu@mango-byte.com>
2022-03-17 05:30:00 +0900

    update:change some small things

--- app/add_movie_meaning.py
+++ app/add_movie_meaning.py
@@ -1,10 +1,8 @@
 def calculate_coordinate_information(x,y):
-    if (1500 <= x and x < 2500) and (0 <= y and y < 1500):
+    if (1700 <= x and x < 2300) and (800 <= y and y < 1500):
         return 'Face'
-    # elif (0 <= x and x < 0) and (0 <= y and y < 2160):
-    #     return 'Eye'
-    # elif (0 <= x and x < 0) and (0 <= y and y < 2160):
-    #     return 'Mouth'
+    elif (1500 <= x and x < 2500) and (0 <= y and y < 800):
+        return 'Body'
     elif (0 <= x and x < 500) and (0 <= y and y < 1000):
         return 'Desk'
     elif (0 <= x and x < 200) and (1000 <= y and y < 2160):
--- app/visualize_result.py
+++ app/visualize_result.py
@@ -1,46 +1,47 @@
 import diagram
 
-# print('\n\n------------------- Gaze Information Plot Setting -------------------')
+print('\n\n------------------- Gaze Information Plot Setting -------------------')
 
-# print("\nWhose gaze information do you want 'BEREAVED FAMILY' or 'NURSE' or 'ALL' ?")
-# user = input("Please enter b/n/a : ")
+print("\nWhose gaze information do you want 'BEREAVED FAMILY' or 'NURSE' or 'ALL' ?")
+user = input("Please enter b/n/a : ")
 
-# print("\nWhich plot time span do you want 'SHORT' or 'LONG' ?")
-# span = input("Please enter s/l : ")
+print("\nWhich plot time span do you want 'SHORT' or 'LONG' ?")
+span = input("Please enter s/l : ")
 
-# if span == 'l':
-#     print("\nWhich plot mode do you want 'LINE' or 'POINT' ?")
-#     mode = input("Please enter l/p : ")
-# else :
-#     mode = 'p'
+if span == 'l':
+    print("\nWhich plot mode do you want 'LINE' or 'POINT' ?")
+    mode = input("Please enter l/p : ")
+else :
+    mode = 'p'
 
-# if user == 'a':
-#     print("\nWhose gaze information do you want 'SEPALATE' or 'OVERLAP' ?")
-#     graph = input("Please enter s/o : ")
-# else :
-#     graph = 's'
+if user == 'a':
+    print("\nWhose gaze information do you want 'SEPALATE' or 'OVERLAP' ?")
+    graph = input("Please enter s/o : ")
+else :
+    graph = 's'
 
-# diagram = diagram.Diagram(user, span, mode, graph)
-# print('User: ' + diagram.user)
-# print('Span: ' + diagram.span)
-# print('Mode: ' + diagram.mode)
-# print('Graph: ' + diagram.graph)
-
-# if user == 'a':
-#     diagram.display_scatter_diagrams()
-# else :
-#     diagram.display_scatter_diagram()
-
-# Let's refactor and add Function annotation
-
-####### FOR TEST #######
-
-diagram = diagram.Diagram('a', 's', 'p', 'o')
-
+diagram = diagram.Diagram(user, span, mode, graph)
 print('User: ' + diagram.user)
 print('Span: ' + diagram.span)
 print('Mode: ' + diagram.mode)
 print('Graph: ' + diagram.graph)
 
+if user == 'a':
+    diagram.display_scatter_diagrams()
+else :
+    diagram.display_scatter_diagram()
+
+
+
+
+####### FOR TEST #######
+
+# diagram = diagram.Diagram('a', 's', 'p', 'o')
+
+# print('User: ' + diagram.user)
+# print('Span: ' + diagram.span)
+# print('Mode: ' + diagram.mode)
+# print('Graph: ' + diagram.graph)
+
 # diagram.display_scatter_diagram()
-diagram.display_scatter_diagrams()
+# diagram.display_scatter_diagrams()
